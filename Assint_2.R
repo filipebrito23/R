14b85be286753f2987f88592296038b0c4d16ef7
Filipe Almeida <filipe.almeidabrito@gmail.com>
2020-09-14 13:36:21 -0300

    mudanca assint

--- Assint_2.R
+++ Assint_2.R
@@ -1,24 +1,24 @@
 require(rmutil)
-a = NULL #variável auxiliar
+a = NULL #variï¿½vel auxiliar
 B=1000 
 n = 5 
-sigma2=1 #variância da normal
-m=2 #média da normal
+sigma2=1 #variï¿½ncia da normal
+m=2 #mï¿½dia da normal
 for(i in 1:B){
-  r = rnorm(n, m, sigma2); #distribuição normal
+  r = rnorm(n, m, sigma2); #distribuiï¿½ï¿½o normal
   a[i]= sum(r)
 }
 
 plot(density(a,adjust=2,cut = 0),lwd=2, col = "blue", lty = 1, ylim = c(0,0.25), xlab = "X", 
-     ylab= "Y", main="Gráfico Ponto de Sela") #gráfico
+     ylab= "Y", main="Grï¿½fico Ponto de Sela") #grï¿½fico
 
 fsn = function(s,n,k,theta){
-  lchap = (s/n - m)/sigma2 #parâmetro
-  K1 = (lchap*m)+((lchap^2)*sigma2/2) #Função geratriz Cumulante
+  lchap = (s/n - m)/sigma2 #parï¿½metro
+  K1 = (lchap*m)+((lchap^2)*sigma2/2) #Funï¿½ï¿½o geratriz Cumulante
   K2 = sigma2 # Cumulante 2
   return(((exp((-(s-n*m)^2)/2*n*sigma2))/sqrt(2*n*pi*sigma2))) #fdp
 }
 
 curve(fsn(x,n,2.5,0.8), lwd=2, col = "red", type="l", add=T) #Ajuste
-legend("topright",legend=c("Expansão Ponto de Sela", "Densidade Empírica"),
+legend("topright",legend=c("Expansï¿½o Ponto de Sela", "Densidade Empï¿½rica"),
        col=c("red","blue"),lwd=2, bty="n") 
